8df8ee12c549de941d92192cb8bd2ff2c852fbe8
MiguelMarinho02 <miguelnogueira02@gmail.com>
2024-09-07 17:55:20 +0100

    added navigation bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,9 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mal_clone/components/app_text_title.dart';
 import 'package:mal_clone/components/drawer.dart';
+import 'package:mal_clone/pages/main_pages/game_list.dart';
+import 'package:mal_clone/pages/main_pages/home.dart';
+import 'package:mal_clone/pages/main_pages/search.dart';
 
 class MyHomePage extends StatefulWidget {
-
   MyHomePage({super.key, required this.user});
 
   final User? user;
@@ -14,6 +16,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int currentPageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,19 +30,46 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         iconTheme: const IconThemeData(color: Colors.white),
       ),
-      drawer: MyDrawer(user: widget.user,),
-      backgroundColor: const Color.fromARGB(242, 13, 13, 14),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "lol",
-              style: TextStyle(color: Colors.white),
-            )
-          ],
-        ),
+      drawer: MyDrawer(
+        user: widget.user,
       ),
+      backgroundColor: const Color.fromARGB(242, 13, 13, 14),
+      body:<Widget>[
+        MyHome(),
+        MySearch(),
+        MyList(),
+      ][currentPageIndex],
+      bottomNavigationBar: NavigationBarTheme(
+        data: const NavigationBarThemeData(
+          labelTextStyle: WidgetStatePropertyAll(
+            TextStyle(color: Colors.white)
+          )
+        ),
+        child: NavigationBar(
+          backgroundColor: const Color.fromARGB(255, 42, 42, 42),
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          indicatorColor: Theme.of(context).colorScheme.primary,
+          selectedIndex: currentPageIndex,
+          destinations: const<Widget>[
+            NavigationDestination(
+              icon: Icon(Icons.home, color: Colors.white,),
+              label: 'Home',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.search, color: Colors.white,),
+              label: 'Search'
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.list_rounded, color: Colors.white,),
+              label: 'My List'
+            ),
+          ]
+        ),  
+      ), 
     );
   }
 }
---
+++ lib/pages/main_pages/game_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyList extends StatelessWidget {
+  const MyList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Lista", style: TextStyle(color: Colors.white),),
+    );
+  }
+}
